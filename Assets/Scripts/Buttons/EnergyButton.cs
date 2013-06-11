d387f8e1b71c7e6a8138dbc660f2c7e1626101f4
Joe Ortega <joeortega79@gmail.com>
2013-06-11 09:27:50 -0700

    Further reduction of 'pause issue' in  Energy Button.

--- Assets/Scripts/Buttons/EnergyButton.cs
+++ Assets/Scripts/Buttons/EnergyButton.cs
@@ -58,40 +58,48 @@ public class EnergyButton : MonoBehaviour
 	public void clickedFA ()
 	{
 		tracker.AddProgressToAchievement ("Energy Boost", 1.0f);
-		StoreInventory.TakeItem (itemId, 1);
         balance--;
-		
-        //balance = StoreInventory.GetItemBalance (itemId);
-        textMesh.text = balance.ToString();
-        textMesh.Commit();
 
         icon.color = Color.gray;
         cooldownActive = !cooldownActive;
 
         timerScript.countDownSeconds += 10;
-        timerScript.DisplayTimer();
 
+        Invoke("changedDisplayFA", 0.5f);
         Invoke("ActivateCooldown", 10); // cooldown lasts 10 secs
 	}
 	
 	public void clickedPM ()
 	{
 		tracker.AddProgressToAchievement ("Energy Boost", 1.0f);
-		StoreInventory.TakeItem (itemId, 1);
         balance--;
-
-        //balance = StoreInventory.GetItemBalance (itemId);
-        textMesh.text = balance.ToString();
-        textMesh.Commit();
 
         icon.color = Color.gray;
 		cooldownActive = true;
 
         colliderScript.lifeCounter++;
-        colliderScript.DisplayLives();
 
+        Invoke("changedDisplayPM", 0.5f);
         Invoke("ActivateCooldown", 10); // cooldown lasts 10 secs
 	}
+
+    void changedDisplayFA()
+    {
+        textMesh.text = balance.ToString();
+        textMesh.Commit();
+
+        timerScript.DisplayTimer();
+        StoreInventory.TakeItem(itemId, 1);
+    }
+
+    void changedDisplayPM()
+    {
+        textMesh.text = balance.ToString();
+        textMesh.Commit();
+
+        colliderScript.DisplayLives();
+        StoreInventory.TakeItem(itemId, 1);
+    }
 	
 	private void ActivateCooldown() {
         icon.color = Color.white;
